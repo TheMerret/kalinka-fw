22eebad37cb5ee2a3bea79bbbb4769d286e92e22
TheMerret <merret@bk.ru>
2022-12-25 18:42:48 +0300

    failed to change testing function working for 3d

--- kalinka-fw/kalinka-fw.ino
+++ kalinka-fw/kalinka-fw.ino
@@ -86,7 +86,7 @@ void reset_sensor_height(byte sensor_id) {
 
 void reset_sensor2_vertical_angle() {
   if (LOGGING) {
-    Serial.println("reset 2 reset vertical rotation");
+    Serial.println("sensor 2 reset vertical rotation");
   }
   float degree;
   if (scannig_direction & SCANNING_VERTICALLY) {
@@ -200,109 +200,176 @@ byte parse_command(byte command, byte prev_state) {
 const float R = 4;
 const float CUBE_SIZE = 3;
 
-float get_y(float x, float scene_radian, float horizontal_radian) {
-    return tan(horizontal_radian + scene_radian) * (x - R * cos(scene_radian)) + R * sin(scene_radian);
-}
-
-float get_x(float y, float scene_radian, float horizontal_radian) {
-    return (y - R * sin(scene_radian)) / tan(horizontal_radian + scene_radian) + R * cos(scene_radian);
-}
-
 struct Point {
     float x;
     float y;
+    float z;
 };
 
 struct Intersections {
     Point arr[2];
 };
 
-struct Intersections get_line_square_intersection(float scene_angle, float horizontal_degree) {
-    float scene_radian = scene_angle * (3.14159265359 / 180);
-    float horizontal_radian = horizontal_degree * (3.14159265359 / 180);
-    struct Intersections intersections;
-    int ind = 0;
-    float x1=-CUBE_SIZE / 2, x2=CUBE_SIZE / 2;
-    float y1=get_y(x1, scene_radian, horizontal_radian), y2=get_y(x2, scene_radian, horizontal_radian);
-    //std::cout << x1 << " " << y1 << " " << x2 << " " << y2 << '\t';
-    if (((-CUBE_SIZE / 2) <= y1) and (y1 <= (CUBE_SIZE / 2))) {
-        struct Point p;
-        p.x = x1;
-        p.y = y1;
-        intersections.arr[ind] = p;
-        ind += 1;
-    }
-    if (((-CUBE_SIZE / 2) <= y2) and (y2 <= (CUBE_SIZE / 2))) {
-        struct Point p;
-        p.x = x2;
-        p.y = y2;
-        intersections.arr[ind] = p;
-        ind += 1;
-    }
-    float y3=-CUBE_SIZE / 2, y4=CUBE_SIZE / 2;
-    float x3=get_x(y3, scene_radian, horizontal_radian), x4=get_x(y4, scene_radian, horizontal_radian);
-    //std::cout << x3 << " " << y3 << " " << x4 << " " << y4 << '\t';
-    if (((-CUBE_SIZE / 2) <= x3) and (x3 <= (CUBE_SIZE / 2))) {
-        struct Point p;
-        p.x = x3;
-        p.y = y3;
-        intersections.arr[ind] = p;
-        ind += 1;
-    }
-    if (((-CUBE_SIZE / 2) <= x4) and (x4 <= (CUBE_SIZE / 2))) {
-        struct Point p;
-        p.x = x4;
-        p.y = y4;
-        intersections.arr[ind] = p;
-        ind += 1;
-    }
-    if (ind == 0) {
-        struct Point p1;
-        p1.x = 1.0/0;
-        p1.y = 1.0/0;
-        intersections.arr[0] = p1;
-        struct Point p2;
-        p2.x = 1.0/0;
-        p2.y = 1.0/0;
-        intersections.arr[1] = p2;
-    }
-    // std::cout << intersections[0][0] << " " << intersections[0][1] << ", " << intersections[1][0] << " " << intersections[1][1] << '\t';
-    return intersections;
+float get_x(float t, float scene_radian, float horizontal_radian) {
+    return R * cos(scene_radian) - cos(scene_radian + horizontal_radian) *  t;
 }
 
-struct Point get_laser_point_on_square(float scene_angle, float horizontal_degree) {
-    struct Intersections points = get_line_square_intersection(scene_angle, horizontal_degree);
+float get_t_by_x(float x, float scene_radian, float horizontal_radian) {
+    return (R * cos(scene_radian) - x) / cos(scene_radian + horizontal_radian);
+}
+
+float get_y(float t, float scene_radian, float horizontal_radian) {
+    return R * sin(scene_radian) - sin(scene_radian + horizontal_radian) *  t;
+}
+
+float get_t_by_y(float y, float scene_radian, float horizontal_radian) {
+    return (R * sin(scene_radian) - y) / sin(scene_radian + horizontal_radian);
+}
+
+float get_z(float t, float height, float vertical_degree) {
+    return height - cos(vertical_degree) *  t;
+}
+
+float get_t_by_z(float z, float height, float vertical_degree) {
+    return (height - z) / cos(vertical_degree);
+}
+
+struct Intersections get_line_cube_intersection(float scene_angle, float horizontal_degree, float height, float vertical_degree) {
+    float scene_radian = scene_angle * (3.14159265359 / 180);
+    float horizontal_radian = horizontal_degree * (3.14159265359 / 180);
+    float vertical_radian = vertical_degree * (3.14159265359 / 180);
+    struct Intersections intersections;
+    int ind = 0;
+    float x, y, z, t;
+    x = -CUBE_SIZE/2;
+    t = get_t_by_x(x, scene_radian, horizontal_radian);
+    y = get_y(t, scene_radian, horizontal_radian);
+    z = get_z(t, height, vertical_radian);
+    if ((-CUBE_SIZE / 2 <= y) and (y <= CUBE_SIZE / 2) and (0 <= z) and (z <= CUBE_SIZE)) {
+        struct Point p;
+        p.x = x;
+        p.y = y;
+        p.z = z;
+        intersections.arr[ind] = p;
+        ind += 1;
+    }
+    y = -CUBE_SIZE/2;
+    t = get_t_by_y(y, scene_radian, horizontal_radian);
+    x = get_x(t, scene_radian, horizontal_radian);
+    z = get_z(t, height, vertical_radian);
+    //std::cout << x << " " << y << " " << z << " " << t << '\t';
+    if ((-CUBE_SIZE / 2 < x) and (x < CUBE_SIZE / 2) and (0 <= z) and (z <= CUBE_SIZE)) {
+        struct Point p;
+        p.x = x;
+        p.y = y;
+        p.z = z;
+        intersections.arr[ind] = p;
+        ind += 1;
+    }
+    z = -CUBE_SIZE/2;
+    t = get_t_by_z(y, height, vertical_radian);
+    x = get_x(t, scene_radian, horizontal_radian);
+    y = get_y(t, scene_radian, horizontal_radian);
+    //std::cout << x << " " << y << " " << z << " " << t << '\t';
+    if ((-CUBE_SIZE / 2 < x) and (x < CUBE_SIZE / 2) and (-CUBE_SIZE / 2 < y) and (y < CUBE_SIZE / 2)) {
+        struct Point p;
+        p.x = x;
+        p.y = y;
+        p.z = z;
+        intersections.arr[ind] = p;
+        ind += 1;
+    }
+    x = CUBE_SIZE/2;
+    t = get_t_by_x(x, scene_radian, horizontal_radian);
+    y = get_y(t, scene_radian, horizontal_radian);
+    z = get_z(t, height, vertical_radian);
+    //std::cout << x << " " << y << " " << z << " " << t << '\t';
+    if ((-CUBE_SIZE / 2 <= y) and (y <= CUBE_SIZE / 2) and (0 <= z) and (z <= CUBE_SIZE)) {
+        struct Point p;
+        p.x = x;
+        p.y = y;
+        p.z = z;
+        intersections.arr[ind] = p;
+        ind += 1;
+    }
+    y = CUBE_SIZE/2;
+    t = get_t_by_y(y, scene_radian, horizontal_radian);
+    x = get_x(t, scene_radian, horizontal_radian);
+    z = get_z(t, height, vertical_radian);
+    //std::cout << x << " " << y << " " << z << " " << t << '\t';
+    if ((-CUBE_SIZE / 2 < x) and (x < CUBE_SIZE / 2) and (0 <= z) and (z <= CUBE_SIZE)) {
+        struct Point p;
+        p.x = x;
+        p.y = y;
+        p.z = z;
+        intersections.arr[ind] = p;
+        ind += 1;
+    }
+    z = CUBE_SIZE/2;
+    t = get_t_by_z(y, height, vertical_radian);
+    x = get_x(t, scene_radian, horizontal_radian);
+    y = get_y(t, scene_radian, horizontal_radian);
+    //std::cout << x << " " << y << " " << z << " " << t << '\t';
+    if ((-CUBE_SIZE / 2 < x) and (x < CUBE_SIZE / 2) and (-CUBE_SIZE / 2 < y) and (y < CUBE_SIZE / 2)) {
+        struct Point p;
+        p.x = x;
+        p.y = y;
+        p.z = z;
+        intersections.arr[ind] = p;
+        ind += 1;
+    }
+    if (ind != 2) {
+        struct Point p1;
+        p1.x = 1/0;
+        p1.y = 1/0;
+        p1.z = 1/0;
+        intersections.arr[0] = p1;
+        struct Point p2;
+        p2.x = 1/0;
+        p2.y = 1/0;
+        p2.z = 1/0;
+        intersections.arr[1] = p2;
+    }
+    return intersections;
+};
+
+struct Point get_laser_point_on_cube(float scene_angle, float horizontal_degree, float height, float vertical_degree) {
+    struct Intersections points = get_line_cube_intersection(scene_angle, horizontal_degree, height, vertical_degree);
     float scene_radian = scene_angle * (3.14159265359 / 180);
     struct Point p1 = points.arr[0];
     struct Point p2 = points.arr[1];
     struct Point p;
     p.x = R * cos(scene_radian);
     p.y = R * sin(scene_radian);
+    p.z = height;
     //std::cout << std::hypot(p1[0] - p[0], p1[1] - p[1]) << " " << std::hypot(p2[0] - p[0], p2[1] - p[1]) << '\t';
-    if (hypot(p1.x - p.x, p1.y - p.y) < hypot(p2.x - p.x, p2.y - p.y)) {
+    if (sqrt(pow(p1.x - p.x, 2) +
+                  pow(p1.y - p.y, 2) +
+                  pow(p1.z - p.z, 2)) < sqrt(pow(p2.x - p.x, 2) +
+                                                       pow(p2.y - p.y, 2) +
+                                                       pow(p2.z - p.z, 2))) {
         return p1;
     } else {
         return p2;
     }
 }
 
-float get_distance_to_square(float scene_angle, float horizontal_degree) {
-    struct Point p = get_laser_point_on_square(scene_angle, horizontal_degree);
+float get_distance_to_cube(float scene_angle, float horizontal_degree, float height, float vertical_degree) {
+    struct Point p = get_laser_point_on_cube(scene_angle, horizontal_degree, height, vertical_degree);
+    //std::cout << p.x << " " << p.y << '\t';
     float scene_radian = scene_angle * (3.14159265359 / 180);
-    float horizontal_radian = horizontal_degree * (3.14159265359 / 180);
-    float distance = hypot(p.x - R * cos(scene_radian), p.y - R * sin(scene_radian));
-    return distance;
-}
-
-float get_distance_to_cube(float degrees, float height, float horizontal_degree, float vertical_degree) {
-    float distance = get_distance_to_square(degrees, horizontal_degree);
+    struct Point origin;
+    origin.x = R * cos(scene_radian);
+    origin.y = R * sin(scene_radian);
+    origin.z = height;
+    float distance = sqrt(pow(p.x - origin.x, 2) + pow(p.y - origin.y, 2) + pow(p.z - origin.z, 2));
     return distance;
 }
 
 struct SensorsData capture_sensors_test() {
   SensorsData sd;
-  sd.sensor1.distance = get_distance_to_cube(scanning_state.scene_angle, scanning_state.sensor1.height, scanning_state.sensor1.horizontal_degree, scanning_state.sensor1.vertical_degree);
-  sd.sensor2.distance = get_distance_to_cube(scanning_state.scene_angle + 180.0, scanning_state.sensor2.height, scanning_state.sensor2.horizontal_degree, scanning_state.sensor2.vertical_degree);
+  sd.sensor1.distance = get_distance_to_cube(scanning_state.scene_angle, scanning_state.sensor1.horizontal_degree, scanning_state.sensor1.height, scanning_state.sensor1.vertical_degree + 90); 
+  sd.sensor2.distance = get_distance_to_cube(scanning_state.scene_angle + 180.0, scanning_state.sensor2.horizontal_degree, scanning_state.sensor2.height, scanning_state.sensor2.vertical_degree + 90);
   return sd;
 }
 
