bf35baf19bc4a4e7c384427243b5074678cc464b
TheMerret <merret@bk.ru>
2023-02-03 14:40:47 +0300

    servo fix

--- kalinka-fw/kalinka-fw.ino
+++ kalinka-fw/kalinka-fw.ino
@@ -1,14 +1,14 @@
-#define SENSOR1_HEIGHT_PIN_STEP 3
-#define SENSOR1_HEIGHT_PIN_DIR 6
-#define SENSOR1_HORIZONTAL_PIN 2
-#define SENSOR1_VERTICAL_PIN 17
-#define SENSOR1_SENSOR_PIN 5
+#define SENSOR1_HEIGHT_PIN_STEP   3   //stepper pin step
+#define SENSOR1_HEIGHT_PIN_DIR    6   //stepper pin dir
+#define SENSOR1_HORIZONTAL_PIN    2   //servo pin
+#define SENSOR1_VERTICAL_PIN      17  //
+#define SENSOR1_SENSOR_PIN        5   //
 
-#define SENSOR2_HEIGHT_PIN_STEP 6
-#define SENSOR2_HEIGHT_PIN_DIR 7
-#define SENSOR2_HORIZONTAL_PIN 8
-#define SENSOR2_VERTICAL_PIN 14
-#define SENSOR2_SENSOR_PIN 15
+#define SENSOR2_HEIGHT_PIN_STEP   6   //stepper pin step
+#define SENSOR2_HEIGHT_PIN_DIR    7   //stepper pin dir
+#define SENSOR2_HORIZONTAL_PIN    3   //servo pin
+#define SENSOR2_VERTICAL_PIN      14  //
+#define SENSOR2_SENSOR_PIN        15  //
 
 #define SCANER_SCENE_PIN 11
 
@@ -23,19 +23,23 @@ Scanner scanner(SCANER_SCENE_PIN, sensor1, sensor2);
 void setup() {
   Serial.begin(9600);
   while (!Serial);
-  Serial.println("heh");
-  Serial.write(scanner.getHandshake());
+  Serial.println("begin");
+
+  Serial.println("end");
+  //Serial.write(scanner.getHandshake());
 }
 
 
 void loop() {
-  if (Serial.available() > 0) {
+  /*
+    if (Serial.available() > 0) {
     byte command = Serial.read();
     scanner.parseCommand(command);
-  }
-  if (scanner.pointsAvailable() == 10) {
+    }
+    if (scanner.pointsAvailable() == 10) {
     writeBuffer(scanner.toBytes(), scanner.bytesLen());
     scanner.clear();
-  }
-  scanner.next();
+    }
+    scanner.next();
+  */
 }
